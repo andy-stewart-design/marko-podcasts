edc65045b6c8e1a771144da36e617e76c061ca1d
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2024-03-10 13:06:47 -0400

    grid complete

---
+++ src/constants/mockdata.ts
@@ -0,0 +1,48 @@
+export const mockdata = [
+  {
+    id: 360084272,
+    title: "The Joe Rogan Experience",
+    artist: "Joe Rogan",
+    images: {
+      xl: "https://is1-ssl.mzstatic.com/image/thumb/Podcasts116/v4/a6/36/c6/a636c689-ad3a-a71c-3486-a5f51ae7a9f5/mza_13284170885507902541.jpg/600x600bb.jpg",
+      lg: "https://is1-ssl.mzstatic.com/image/thumb/Podcasts116/v4/a6/36/c6/a636c689-ad3a-a71c-3486-a5f51ae7a9f5/mza_13284170885507902541.jpg/100x100bb.jpg",
+      md: "https://is1-ssl.mzstatic.com/image/thumb/Podcasts116/v4/a6/36/c6/a636c689-ad3a-a71c-3486-a5f51ae7a9f5/mza_13284170885507902541.jpg/60x60bb.jpg",
+      sm: "https://is1-ssl.mzstatic.com/image/thumb/Podcasts116/v4/a6/36/c6/a636c689-ad3a-a71c-3486-a5f51ae7a9f5/mza_13284170885507902541.jpg/30x30bb.jpg",
+    },
+    appleURL:
+      "https://podcasts.apple.com/us/podcast/the-joe-rogan-experience/id360084272?uo=4",
+    rssURL: "https://feeds.megaphone.fm/GLT1412515089",
+    genres: ["Comedy", "Podcasts"],
+  },
+  {
+    id: 1734273640,
+    title: "Archetypes",
+    artist: "Archewell Audio",
+    images: {
+      xl: "https://is1-ssl.mzstatic.com/image/thumb/Podcasts122/v4/1e/6f/f3/1e6ff37d-ad5f-5446-85ff-419affbe3c2a/mza_16835047650982566496.jpg/600x600bb.jpg",
+      lg: "https://is1-ssl.mzstatic.com/image/thumb/Podcasts122/v4/1e/6f/f3/1e6ff37d-ad5f-5446-85ff-419affbe3c2a/mza_16835047650982566496.jpg/100x100bb.jpg",
+      md: "https://is1-ssl.mzstatic.com/image/thumb/Podcasts122/v4/1e/6f/f3/1e6ff37d-ad5f-5446-85ff-419affbe3c2a/mza_16835047650982566496.jpg/60x60bb.jpg",
+      sm: "https://is1-ssl.mzstatic.com/image/thumb/Podcasts122/v4/1e/6f/f3/1e6ff37d-ad5f-5446-85ff-419affbe3c2a/mza_16835047650982566496.jpg/30x30bb.jpg",
+    },
+    appleURL:
+      "https://podcasts.apple.com/us/podcast/archetypes/id1734273640?uo=4",
+    rssURL:
+      "https://www.omnycontent.com/d/playlist/796469f9-ea34-46a2-8776-ad0f015d6beb/fc345980-bd06-455a-aeac-b11c0188880d/14ceb02e-b130-4c2e-bce8-b11c01888829/podcast.rss",
+    genres: ["Society & Culture", "Podcasts", "History"],
+  },
+  {
+    id: 1726465829,
+    title: "The Rise and Fall of Ruby Franke",
+    artist: "Law&Crime | Wondery",
+    images: {
+      xl: "https://is1-ssl.mzstatic.com/image/thumb/Podcasts112/v4/d1/85/fb/d185fb00-9548-6fc1-bc0d-925073a9db10/mza_9214888555685992388.jpeg/600x600bb.jpg",
+      lg: "https://is1-ssl.mzstatic.com/image/thumb/Podcasts112/v4/d1/85/fb/d185fb00-9548-6fc1-bc0d-925073a9db10/mza_9214888555685992388.jpeg/100x100bb.jpg",
+      md: "https://is1-ssl.mzstatic.com/image/thumb/Podcasts112/v4/d1/85/fb/d185fb00-9548-6fc1-bc0d-925073a9db10/mza_9214888555685992388.jpeg/60x60bb.jpg",
+      sm: "https://is1-ssl.mzstatic.com/image/thumb/Podcasts112/v4/d1/85/fb/d185fb00-9548-6fc1-bc0d-925073a9db10/mza_9214888555685992388.jpeg/30x30bb.jpg",
+    },
+    appleURL:
+      "https://podcasts.apple.com/us/podcast/the-rise-and-fall-of-ruby-franke/id1726465829?uo=4",
+    rssURL: "https://rss.art19.com/the-rise-and-fall-of-ruby-franke",
+    genres: ["True Crime", "Podcasts", "Society & Culture"],
+  },
+];
--- src/utils/get-shows.ts
+++ src/utils/get-shows.ts
@@ -1,7 +1,91 @@
+interface Podcast {
+  wrapperType: "track";
+  kind: "podcast";
+  artistId: number;
+  collectionId: number;
+  trackId: number;
+  artistName: string;
+  collectionName: string;
+  trackName: string;
+  collectionCensoredName: string;
+  trackCensoredName: string;
+  artistViewUrl: string;
+  collectionViewUrl: string;
+  feedUrl: string;
+  trackViewUrl: string;
+  artworkUrl30: string;
+  artworkUrl60: string;
+  artworkUrl100: string;
+  collectionPrice: number;
+  trackPrice: number;
+  collectionHdPrice: number;
+  releaseDate: string;
+  collectionExplicitness: "notExplicit";
+  trackExplicitness: "explicit";
+  trackCount: number;
+  trackTimeMillis: number;
+  country: string;
+  currency: string;
+  primaryGenreName: string;
+  contentAdvisoryRating: "Explicit";
+  artworkUrl600: string;
+  genreIds: number[];
+  genres: string[];
+}
+
 export async function getPopularShows() {
   const response = await fetch(
     "https://rss.applemarketingtools.com/api/v2/us/podcasts/top/10/podcasts.json"
   );
   const json = await response.json();
   return json.feed.results;
+}
+
+export async function getPopularShows2() {
+  const response = await fetch(
+    "https://rss.applemarketingtools.com/api/v2/us/podcasts/top/12/podcasts.json"
+  );
+  const json = await response.json();
+  const appleIDs = json.feed.results.map((show) => show.id);
+
+  const showReseults = appleIDs.map((id) => {
+    return fetch(`https://itunes.apple.com/lookup?id=${id}`).then((res) =>
+      res.json()
+    );
+  });
+
+  const showData = (await Promise.all(showReseults)).map(
+    (show) => show.results
+  );
+
+  return showData.flat().map((show) => formatPodcastData(show));
+}
+
+export async function getShowsByKeyword(query = "technology") {
+  const params = new URLSearchParams();
+  params.set("term", query);
+  params.set("media", "podcast");
+  params.set("limit", "10");
+  const response = await fetch(
+    `https://itunes.apple.com/search?${params.toString()}`
+  );
+  const json = await response.json();
+  return json;
+}
+
+function formatPodcastData(data: Podcast) {
+  return {
+    id: data.trackId,
+    title: data.trackName,
+    artist: data.artistName,
+    images: {
+      xl: data.artworkUrl600,
+      lg: data.artworkUrl100,
+      md: data.artworkUrl60,
+      sm: data.artworkUrl30,
+    },
+    appleURL: data.trackViewUrl,
+    rssURL: data.feedUrl,
+    genres: data.genres.slice(0, 3),
+  };
 }
